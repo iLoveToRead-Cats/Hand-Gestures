f9bc6574ea8f995c9b4832247fd6820dec47b791
iLoveToRead-Cats <75917493+iLoveToRead-Cats@users.noreply.github.com>
2021-07-21 18:49:21 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 prediction_1 = "";
-prediction_2 ="";
 Webcam.set({
     width: 350,
     height:300,
@@ -18,7 +17,7 @@ function take_snapshot() {
 }
 
 console.log('ml5 version:', ml5.version);
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/eolKEqMrQ/model.json');
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/eolKEqMrQ/model.json', modelLoaded);
 
 function modelLoaded() {
     console.log('Model Loaded');
@@ -27,7 +26,32 @@ function modelLoaded() {
 function speak() {
     var synth = window.speechSynthesis;
     speak_data_1 = "The first prediction is " + prediction_1;
-    speak_data_2 = "And the second prediction is " + prediction_2;
-    var utterThis = new SpeechSynthesisUtterance(speak_data_1+speak_data_2);
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1);
     synth.speak(utterThis);
+}
+
+function check() {
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult (error, results) {
+    if (error) {
+        console.error(error);
+    }
+    else {
+        console.log("results");
+        document.getElementById("result_emotion_name").innerHTML= results[0].label;
+        prediction_1 = results[0].label;
+        speak();
+        if (results[0].label == "best") {
+            document.getElementById("update_emoji").innerHTML= "&#128077;";
+        }
+        if (results[0].label == "victory") {
+            document.getElementById("update_emoji").innerHTML= "&#9996;";
+        }
+        if (results[0].label == "amazing") {
+            document.getElementById("update_emoji").innerHTML= "&#128076;";
+        }
+    }
 }
